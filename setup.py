5d0068d7e0cfe51d438a71eb1878e0036efef504
Francisco Forster <francisco.forster@gmail.com>
2022-07-22 11:38:07 -0400

    fixing pip

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Francisco Förster',
     author_email='francisco.forster@gmail.com',
     license='GNU GPLv3',
-    packages=find_namespace_packages(include=["lib.*"]),
+    packages=find_namespace_packages(include=["delight.*"]),
     install_requires=['astropy',
                       'sep',
                       'xarray',
